3cae93b41cd0804096d9880ac1e0ea49b2cf8ded
aigerim678 <aigerimsabyrkhan393@gmail.com>
2024-11-29 18:26:20 +0500

    ендпойнт для получения ученика

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import Optional
+
+from models import Student, Base
+from schemas import StudentSchema
+from database import engine, get_db
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/student/{student_id}", response_model=StudentSchema)
+async def get_student(student_id: int, db: Session = Depends(get_db)):
+    student = db.query(Student).filter(Student.id == student_id).first()
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
+    
 
