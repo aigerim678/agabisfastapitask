9197a65772e94de8ed30ed2d6890b838474a328f
aigerim678 <aigerimsabyrkhan393@gmail.com>
2024-11-30 14:56:26 +0500

    эндпойнт для получения всех учеников

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import Optional
+from typing import List, Optional
 
 from models import Student, Base
 from schemas import ScoreUpdateSchema, StudentSchema
@@ -28,5 +28,13 @@ async def update_student_score(student_id: int, score_update: ScoreUpdateSchema,
     student.score = score_update.score
     db.commit()
     return student
+
+
+@app.get("/students", response_model=List[StudentSchema])
+async def get_students(db: Session = Depends(get_db)):
+    students = db.query(Student).all()
+    if not students:
+        raise HTTPException(status_code=404, detail="Students not found")
+    return students
     
 
