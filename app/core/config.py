611458fa51a5b34a1379d004922100adb863f8d6
aigerim678 <aigerimsabyrkhan393@gmail.com>
2024-12-01 15:55:55 +0500

    config read from env,

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from pydantic import PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
@@ -18,6 +18,12 @@ class DatabaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__"
+    )
     run: RunConfig = RunConfig()
     api: StudentPrefix = StudentPrefix()
     db: DatabaseConfig
